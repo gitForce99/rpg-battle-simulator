3653359054dfa0a1f44f7b33bd3294c8c3304df3
Maxwell Forsyth <maxjforsyth@gmail.com>
2021-01-18 00:02:57 -0500

    Adding overall simulator class and a class for the specific attack

---
+++ SpecificAttack.java
@@ -0,0 +1,31 @@
+class SpecificAttack{
+    private int magicOrPhysical;
+    private int attackPower;
+    private int mandatoryMultitarget;
+    private int multitargetAllowed;
+    private char elementalAffinity;
+
+    public SpecificAttack()
+
+
+    public int getMagicOrPhysical(){
+        return magicOrPhysical;
+    }
+
+    public int getAttackPower(){
+        return attackPower;
+    }
+
+    public int getMandatoryMultitarget(){
+        return mandatoryMultitarget;
+    }
+
+    public int getMultitargetAllowed(){
+        return multitargetAllowed;
+    }
+
+    public char elementalAffinity(){
+        return elementalAffinity;
+    }
+
+}
