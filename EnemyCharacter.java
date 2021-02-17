4d4483fa593a7816e53001cdc8e8c6b52403c206
Maxwell Forsyth <maxjforsyth@gmail.com>
2021-02-17 00:26:51 -0500

    added some basic constructors for EnemyCharacter and PlayerCharacter

--- EnemyCharacter.java
+++ EnemyCharacter.java
@@ -1,9 +1,17 @@
 class EnemyCharacter extends Combatant.java {
 
+    private ArrayList availableAttacks;
+    private String enemyType;
+    private String enemyName;
+
+    public EnemyCharacter(String name, String type, ArrayList availableAttacks) {
+        self.availableAttacks = availableAttacks;
+        self.enemyType = type;
+        self.enemyName = name;
+    }
 
 
 
 
 
-    
 }
--- PlayerCharacter.java
+++ PlayerCharacter.java
@@ -1,10 +1,15 @@
 class PlayerCharacter extends Combatant {
 
+    private Job jobType;
+    private String name;
+
+
+    public PlayerCharacter(String name, Job jobType) {
+        self.name = name;
+        self.jobType = jobType;
+    }
 
 
 
 
-
-
-    
 }
